a8d82fa5563f3beb40aa6a5968eb9ead19209951
luminous <masha4russia@gmail.com>
2025-03-07 01:08:58 +0300

    ВСЕ

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ port: 9000 });
 
 app.use(express.static('public'));
 
@@ -22,31 +22,43 @@ const schema = buildSchema(`
     type Category {
         id: ID!
         name: String!
+        products: [Product]
     }
 
     type Product {
         id: ID!
         name: String!
+        price: Int!
+        description: String!
         categoryIds: [ID!]!
     }
 
     type Query {
-        products: [Product]
         categories: [Category]
     }
 `);
 
-// Корневой резолвер
-const root = {
-    products: () => {
-        const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
-        return data.products;
-    },
+
+let root = {
     categories: () => {
         const data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
-        return data.categories;
+        let categoriesRes = [];
+        for(let category of data.categories) {
+            category.products = [];
+            for(let Product of data.products ){
+                for(let categoryId of Product.categoryIds) {
+                    if(categoryId === category.id) {
+                        category.products.push(Product)
+                        break
+                    }
+                }
+
+            }
+            categoriesRes.push(category)
+        }
+        return categoriesRes
     }
-};
+}
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
@@ -55,15 +67,25 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 // WebSocket соединение
-wss.on('connection', (ws) => {
+let clients = []
+wss.on('connection', (client) => {
     console.log('Client connected');
+    clients.push(client)
 
-    ws.on('message', (message) => {
+    client.on('message', (message) => {
+        const data = JSON.parse(message)
+        for(const Client of clients) {
+            Client.send(JSON.stringify(data));
+        }
         console.log(`Received message: ${message}`);
-        ws.send(`Echo: ${message}`);
     });
 
-    ws.on('close', () => {
+    client.on('close', () => {
+        const index = clients.indexOf(client);
+        if (index !== -1) {
+            clients.splice(index, 1);
+        }
+
         console.log('Client disconnected');
     });
 });
